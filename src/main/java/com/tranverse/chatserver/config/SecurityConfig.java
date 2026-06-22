c9e391fbfe18eb1bbdcfcc461b23285da510ed96
Tran Ho <tranhonguyen.work@gmail.com>
2026-06-22 08:22:13 +0700

    feat(docs): add Swagger OpenAPI configuration

--- src/main/java/com/tranverse/chatserver/config/SecurityConfig.java
+++ src/main/java/com/tranverse/chatserver/config/SecurityConfig.java
@@ -33,9 +33,10 @@ import java.util.Arrays;
 public class SecurityConfig {
     private static final String[] PUBLIC_ENDPOINTS = {
             "/api/v1/auth/**",
-            "/api/v1/user/register",
             "/swagger-ui/**",
+            "/swagger-ui.html",
             "/v3/api-docs/**",
+            "/v3/api-docs.yaml"
     };
 
     private final JwtDecoder jwtDecoder;
--- src/main/java/com/tranverse/chatserver/dto/response/auth/LoginResponse.java
+++ src/main/java/com/tranverse/chatserver/dto/response/auth/LoginResponse.java
@@ -1,11 +1,13 @@
 package com.tranverse.chatserver.dto.response.auth;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @Builder
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class LoginResponse {
     private String accessToken;
     private String refreshToken;
